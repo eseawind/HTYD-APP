c2cda5568a685d91a24260efa6daab8126bad481
Fan <jishuo1213@126.com>
2015-01-14 11:29:40 +0800

    New File

---
+++ src/xdj/XDJMainActivity.java
@@ -0,0 +1,14 @@
+package xdj;
+
+import android.app.Fragment;
+import main.activity.SimpleActivityWithMenu;
+
+public class XDJMainActivity extends SimpleActivityWithMenu {
+
+	@Override
+	protected Fragment createFragment() {
+		
+		return null;
+	}
+
+}
